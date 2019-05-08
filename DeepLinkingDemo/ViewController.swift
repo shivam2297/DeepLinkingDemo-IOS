ad8091069504a7e6bb7c4e48bb66db4a4f610db1
developer1 <sshrivastav@contentactive.com>
2019-05-08 14:06:54 +0530

    init commit

---
+++ DeepLinkingDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeepLinkingDemo
+//
+//  Created by Daffolspmac-67 on 08/05/19.
+//  Copyright © 2019 Daffodil Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
